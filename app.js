e6c79655b6e055d6823539e9b1f64d95a487e4d7
KimDowi <rlaehdnl@gmail.com>
2023-09-27 22:36:11 +0900

    2023-09-27

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const title = document.querySelector(".hello h1");
 
-title.innerText = "Hello";
+function handleTitleClick(){
+    console.log("title was clicked!");
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
