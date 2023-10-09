f3888b2cb8cfc58317173e5f38fef68ddd64f2f2
KimDowi <rlaehdnl@gmail.com>
2023-10-09 20:02:59 +0900

    2023-10-09

--- app.js
+++ app.js
@@ -2,39 +2,16 @@ const h1 = document.querySelector(".hello h1");
 
 console.dir(h1);
 
-function handleh1Click(){
-    h1.style.color = "blue";
+function handleTitleClick(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor == "blue"){
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
 
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
-
-h1.onclick = handleh1Click;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
