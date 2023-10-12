a00024d1824c430237e0d789991951e3cf49ecf3
KimDowi <rlaehdnl@gmail.com>
2023-10-12 19:57:58 +0900

    2023-10-12

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,11 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
 
 function deleteToDo(event) {
     const deleteLi = event.target.parentElement;
@@ -23,7 +28,9 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newToDo)
+    toDos.push(newToDo);
+    paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
