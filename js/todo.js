af6322558f67df5224b9aa08b63808bc8b9bc9fa
KimDowi <rlaehdnl@gmail.com>
2023-10-12 19:28:43 +0900

    2023-10-12

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const deleteLi = event.target.parentElement;
+    deleteLi.remove();
+}
+
 function paintToDo(newToDo) {
     const createLi = document.createElement("li");
     const createSpan = document.createElement("span");
-    createLi.appendChild(createSpan);
     createSpan.innerText = newToDo;
+    const createButton = document.createElement("button");
+    createButton.innerText = "✖️";
+    createButton.addEventListener("click", deleteToDo)
+    createLi.appendChild(createSpan);
+    createLi.appendChild(createButton);
     toDoList.appendChild(createLi);
 }
 
